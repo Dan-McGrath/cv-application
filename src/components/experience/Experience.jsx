e0bf3c4a7426003a07e50bfeea8d2f1629ca084d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-28 17:07:47 -0400

    Finish experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,6 +3,7 @@ import ExperienceForm from "../form/ExperienceForm.jsx"
 export default function Experience() {
     return (
         <section className="experience">
+            <h2>Experience</h2>
             <ExperienceForm />
         </section>
     )
