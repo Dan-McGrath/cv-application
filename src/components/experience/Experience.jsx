778e381bb7deba11df981893509f002b000c0714
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-28 16:31:01 -0400

    Add files and some basic code

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,9 @@
+import ExperienceForm from "../form/ExperienceForm.jsx"
+
+export default function Experience() {
+    return (
+        <section className="experience">
+            <ExperienceForm />
+        </section>
+    )
+}
