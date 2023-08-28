e89c9a4a9e746b88be5097b378e6e5d90eac7809
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-28 17:33:57 -0400

    Fix experience section

--- src/components/form/ExperienceForm.jsx
+++ src/components/form/ExperienceForm.jsx
@@ -4,40 +4,47 @@ import Button from "./button.jsx";
 
 export default function ExperienceForm() {
     const [isSubmitted, setIsSubmitted] = useState(false);
-    const [schoolName, setSchoolName] = useState('');
-    const [degree, setDegree] = useState('');
-    const [graduationDate, setGraduationDate] = useState('');
-
-    const schoolNameHandler = (e) => setSchoolName(e.target.value);
-    const degreeHandler = (e) => setDegree(e.target.value);
-    const graduationDateHandler = (e) => setGraduationDate(e.target.value)
+    const [companyName, setCompanyname] = useState('');
+    const [positionTitle, setPositionTitle] = useState('')
+    const [responsibilities, setResponsibilities ] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
 
     const submitHandler = (e) => {
         e.preventDefault();
         setIsSubmitted(!isSubmitted)
-    }
+    };
+    
+    const editHandler = () => setIsSubmitted(!isSubmitted);
 
-    const editHandler = (e) => {
-        e.preventDefault(e);
-        setIsSubmitted(!isSubmitted);
-    }
+    const companyNameHandler = (e) => setCompanyname(e.target.value)
+    const positionTitleHandler = (e) => setPositionTitle(e.target.value);
+    const responsibilitiesHandler = (e) => setResponsibilities(e.target.value);
+    const startDateHnadler = (e) => setStartDate(e.target.value);
+    const endDateHnadler = (e) => setEndDate(e.target.value);
 
     return (
         isSubmitted ? 
         <>
-            <p>{schoolName}</p>
-            <p>{degree}</p>
-            <p>{graduationDate}</p>
-            <Button onClick={editHandler} />
+        <p>{companyName}</p>
+        <p>{positionTitle}</p>
+        <p>{responsibilities}</p>
+        <p>{startDate}</p>
+        <p>{endDate}</p>
+        <Button onClick={editHandler}/>
         </> :
         <form onSubmit={submitHandler}>
-            <Label htmlFor='school-name' text='School Name: ' />
-            <Input type='text' name='school-name' idName='school-name' text='School Name' value={schoolName} handler={schoolNameHandler}/>
-            <Label htmlFor='degree' text='Degree: ' />
-            <Input type='text' name='degree' idName='degree' text='Degree Name' value={degree} handler={degreeHandler}/>
-            <Label htmlFor='graduation-date' text='Graduation Date: ' />
-            <Input type='date' name='graduation-date' idName='graduation-date' value={graduationDate} handler={graduationDateHandler}/>
-            <input type='submit' />
-        </form>        
+            <Label htmlFor='company-name' text='Company Name'/>
+            <Input type='text' name='company-name' idName='company-name' text='Company Name' handler={companyNameHandler} value={companyName} />
+            <Label htmlFor='position-title' text='Position Title:' />
+            <Input type='text' name='position-title' idName='position-title' text='Position Title' handler={positionTitleHandler} value={positionTitle}  />
+            <Label htmlFor='responsibilities' text='Responsibilities: ' />
+            <textarea type='textarea' name='responsibilities' idName='responsibilities' placeholder='Responsibilities...' handler={responsibilitiesHandler} value={responsibilities}  />
+            <Label htmlFor='start-date' text='Start Date: ' />
+            <Input type='date' name='start-date' idName='start-date' text='00/00' handler={startDateHnadler} value={startDate}  />
+            <Label htmlFor='end-date' text='End Date: ' />
+            <Input type='date' name='end-date' idName='end=date' text='00/00' handler={endDateHnadler} value={endDate}  />
+            <input type="submit" />
+        </form>
     )
-}
+}   
