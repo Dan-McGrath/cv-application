a37bcf07e4aeb1a140a90ec51043a853508b4502
Dan McGrath <mcgrathd3254@yahoo.com>
2023-08-28 16:31:18 -0400

    Finish personal form section

---
+++ src/components/personal/Personal.jsx
@@ -0,0 +1,11 @@
+
+import PersonalForm from "../form/PersonalForm.jsx"
+
+
+export default function Personal() {
+    return (
+        <section className="personal">
+                <PersonalForm />
+        </section>
+    )
+}
